fa54adb2bc72d9a59294ad44f66afb71b949c20f
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-03-06 13:19:27 +0100

    Update and rename backend/src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java to backend/src/main/java/onlus/manager/springbootvuejs/SpringBootVuejsApplication.java

--- backend/src/main/java/onlus/manager/springbootvuejs/SpringBootVuejsApplication.java
+++ backend/src/main/java/onlus/manager/springbootvuejs/SpringBootVuejsApplication.java
@@ -1,4 +1,4 @@
-package de.jonashackt.springbootvuejs;
+package onlus.manager.springbootvuejs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
