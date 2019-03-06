ce9e6e5d6fac89de579f244384f88eb0f570025e
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-03-06 14:16:59 +0100

    Update and rename backend/src/main/java/de/jonashackt/springbootvuejs/hibernate/repository/PersonRepository.java to backend/src/main/java/onlus/manager/springbootvuejs/hibernate/repository/PersonRepository.java

--- backend/src/main/java/onlus/manager/springbootvuejs/hibernate/repository/PersonRepository.java
+++ backend/src/main/java/onlus/manager/springbootvuejs/hibernate/repository/PersonRepository.java
@@ -1,11 +1,11 @@
-package de.jonashackt.springbootvuejs.hibernate.repository;
+package onlus.manager.springbootvuejs.hibernate.repository;
 
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import de.jonashackt.springbootvuejs.hibernate.domain.Person;
+import onlus.manager.springbootvuejs.hibernate.domain.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
