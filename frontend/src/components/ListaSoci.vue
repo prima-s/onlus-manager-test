53b7c364bf2e3931a01acf506defb89e657ad43d
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-03-18 16:10:30 +0100

    Updated ListaSoci vue component

--- frontend/src/components/ListaSoci.vue
+++ frontend/src/components/ListaSoci.vue
@@ -3,37 +3,34 @@
         <span  @click="closeAll()" class="close" title="Close Modal">&times;</span>
     <div class="container">
       <h1>Lista soci</h1>
-      <hr> 
+      <hr>
+
  <br>
- 	<div id="dubkovidze">
+ 	<div id="sociTable">
     <b-table striped hover :items="soci" :fields="fields" />
   </div>
-    </div>     
+    </div>
  <br>
-     		
-		
-		<br/><br/>
-		
-            <button @click="createPDF()" class=" cancelbtn btn-block">Download PDF</button>
-            <button @click="callRestService()" class=" cancelbtn btn-block">Retrieve table</button>
-						<button @click="closeAll()" class=" cancelbtn btn-block">Cancel</button>
-					
-          
 
-				
+
+		<br/><br/>
+
+            <button @click="createPDF()" class="cancelbtn btn-block">Download PDF</button>
+			<button @click="closeAll()" class="cancelbtn btn-block">Cancel</button>
+
+
+
+
 </form>
 </template>
 <script scoped>
 import { AXIOS } from "./http-common";
 export default {
-
   data() {
     return {
-      msg: "HowTo call REST-Services:",
       response: [],
       errors: [],
-      value: 75,
-         fields: [
+      fields: [
           {
             key: 'numTessera',
             label:'Numero di tessera',
@@ -50,18 +47,23 @@ export default {
             sortable: true
           }
          ],
-        soci:null
+      soci:null
     };
+  },
+  created() {
+       AXIOS.get(`/soci`)
+               .then(response => {
+                   this.soci=response.data;
+                 })
+                 .catch(e => {
+                   this.errors.push(e)
+                 })
   },
    mounted() {
     let recaptchaScript = document.createElement('script');
     recaptchaScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js');
     document.head.appendChild(recaptchaScript);
-    /*var recaptchaScript2 = document.createElement('script2');
-    recaptchaScript2.setAttribute('src2', 'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.0.13/jspdf.plugin.autotable.min.js');
-    document.head.appendChild(recaptchaScript2)*/
     },
-   
   methods: {
        closeAll(){
       for(var i=0;i<document.getElementsByClassName('modal').length;i++){
@@ -69,47 +71,18 @@ export default {
       }
     },
     createPDF () {
-    /*var pdf = new jsPDF("p", "pt", "a4");
-	  pdf.addHTML($('#dubkovidze'), 15, 15, function() {
-    pdf.save('dubkovidze.pdf');});*/
-    
-    /*let pdfName = 'test'; 
-    let tabella=document.getElementById('dubkovidze');
-    var doc = new jsPDF();
-    doc.text(tabella, 10, 10);
-    doc.save(pdfName + '.pdf');*/
-    /*let vm=this;
-      let columns=[
-        {title:'nome', dataKey:'nome'},
-        {title:'cognome',dataKey:'cognome'}
 
-
-      ],
-      var doc=new jsPDF('p','pt');
-      doc.autoTable(columns,v*/
-    let pdfName = 'test'; 
+    let pdfName = 'test';
     var doc = new jsPDF();
     for (var i=0;i<this.soci.length;i++){
-    doc.text(this.soci[i]['nome'] + '  ' + this.soci[i]['cognome'], 10, 10 + 10*i)
+    doc.text('NUMERO TESSERA          NOME                   COGNOME\n\n' +
+    this.soci[i]['numTessera']  + '\t\t\t\t - \t  ' + this.soci[i]['nome'] + ' \t\t\t ' + this.soci[i]['cognome'], 10, 10 + 10*i)
     }
-    doc.save(ListaDeiSoci + '.pdf');
-    /*doc.text(this.soci[0]['nome'], 10, 10);
-    doc.save(pdfName + '.pdf');*/
-    
-      /*let vm=this;
-      let columns=[
-        {title:'nome', dataKey:'nome'},
-        {title:'cognome',dataKey:'cognome'}
-
-
-      ],
-      var doc=new jsPDF('p','pt');
-      doc.autoTable(columns,vm.info);
-      doc.save('info.pdf');*/
+    doc.save('ListaSoci' + '.pdf');
   },
-    
+
     // Fetches posts when the component is created.
-    callRestService() {
+    retrieveTable() {
       AXIOS.get(`/soci`)
         .then(response => {
             this.soci=response.data;
@@ -117,14 +90,10 @@ export default {
           .catch(e => {
             this.errors.push(e)
           })
-    },
-    onclick:function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    }
+
   }
-}
-  }
-  
+
 };
 </script>
 <style scoped>
@@ -137,17 +106,14 @@ button {
   width: 50% ;
   opacity: 0.9;
 }
-
 button:hover {
   opacity: 1;
 }
-
 /* Extra styles for the cancel button */
 .cancelbtn {
   padding: 14px 20px;
   background-color: #f44336;
 }
-
 /* Float cancel and signup buttons and add an equal width */
 .cancelbtn{
   position: relative;
@@ -155,4 +121,3 @@ button:hover {
   width: 50%;
 }
 </style>
-
