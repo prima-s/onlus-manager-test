2efa36c7beabcd43cd7a3a5e55f39fa46e009f68
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-03-21 11:29:27 +0100

    Added AggiornaPassword and Voci controllers

--- backend/src/main/java/onlus/manager/springbootvuejs/controller/BackendController.java
+++ backend/src/main/java/onlus/manager/springbootvuejs/controller/BackendController.java
@@ -1,7 +1,10 @@
 package onlus.manager.springbootvuejs.controller;
 
+import onlus.manager.springbootvuejs.hibernate.domain.Credenziali;
 import onlus.manager.springbootvuejs.hibernate.domain.Socio;
 import onlus.manager.springbootvuejs.hibernate.domain.TipoVoce;
+import onlus.manager.springbootvuejs.hibernate.domain.Voce;
+import onlus.manager.springbootvuejs.hibernate.service.BilancioService;
 import onlus.manager.springbootvuejs.hibernate.service.LoginService;
 import onlus.manager.springbootvuejs.hibernate.service.SocioService;
 import onlus.manager.springbootvuejs.hibernate.service.VoceService;
@@ -11,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,6 +34,9 @@ public class BackendController {
     @Autowired
     private SocioService socioService;
 
+    @Autowired
+    private BilancioService bilancioService;
+
     @RequestMapping(path = "/restlogin", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     public @ResponseBody String accessoSocio (@RequestParam("utente") String requestNomeUtente, @RequestParam("password")  String requestPassword) {
@@ -37,7 +45,7 @@ public class BackendController {
         Socio socio = loginService.getSocio(loginService.verificaUtenze(requestNomeUtente, requestPassword));
         if(socio.getNumTessera()!=0){
             System.out.println(loginService.getLivelloAmministrazione(socio));
-            System.out.println(Arrays.toString(socio.getRuolo().split("")));
+            System.out.println(Arrays.toString(socio.getRuolo().split(",")));
             return loginService.getLivelloAmministrazione(socio);
         }
         else{
@@ -65,11 +73,26 @@ public class BackendController {
         return persistedSocio;
     }
 
-    @RequestMapping(path="/person/{numTessera}", method = RequestMethod.DELETE)
+    // @RequestMapping(path="/person/{numTessera}", method = RequestMethod.DELETE)
+    // @ResponseStatus(HttpStatus.OK)
+    // public void deleteSocioById(@PathVariable("numTessera") Integer numTessera) {
+    //     LOG.info("Delete socio con numero tessera " + numTessera + " from database.");
+    //     socioService.deleteSocio(numTessera);
+    // }
+
+    @RequestMapping(path="/aggiornaPassword", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public void deleteSocioById(@PathVariable("numTessera") Integer numTessera) {
-        LOG.info("Delete socio con numero tessera " + numTessera + " from database.");
-        socioService.deleteSocio(numTessera);
+    public boolean updatePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
+        
+        Credenziali cred = socioService.aggiornaPassword(oldPassword, newPassword);
+        if (cred != null) {
+            LOG.info("Cambiata password: da '" + oldPassword + "' a '" + newPassword + "'" );
+            return true;
+        } else {
+            LOG.info("La password '" + oldPassword + "' non è stata trovata" );
+            return false;
+        }
+       
     }
 
     @RequestMapping(path="/soci", method = RequestMethod.GET)
@@ -78,12 +101,51 @@ public class BackendController {
         return socioService.getAllSoci();
     }
 
+    @RequestMapping(path="/voci", method = RequestMethod.GET)
+    public @ResponseBody
+    List<TipoVoce> getVoci() {
+        return voceService.getVoci();
+    }
+
     @RequestMapping(path="/aggiungiVoce", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public void addVoce(@RequestParam String voce) {
         LOG.info("Aggiunta voce " + voce );
         TipoVoce tv = new TipoVoce(voce);
-        voceService.aggiungiVoceInBilancio(tv);
+        voceService.aggiungiVoceDiBilancio(tv);
+    }
+
+    @RequestMapping(path="/rimuoviVoce", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void addVoce(@RequestParam Integer id) {
+        LOG.info("Rimossa voce con id " + id );
+        voceService.rimuoviVoceDiBilancio(id);
+    }
+
+    @RequestMapping(path="/inserisciVoce", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void inserisciVoce(@RequestParam String tipoVoce, @RequestParam BigDecimal importoVoce, @RequestParam String dataVoce, @RequestParam String noteVoce ) {
+        LOG.info("Inserita voce: " + tipoVoce + " " + importoVoce + " " + dataVoce + " " + noteVoce );
+        TipoVoce tv = new TipoVoce(tipoVoce);
+        LocalDate date = LocalDate.parse(dataVoce);
+        Voce v = new Voce(tv, importoVoce, date, noteVoce);
+        bilancioService.aggiungiVoceInBilancio(v);
+    }
+
+    @RequestMapping(path="/inserisciBilancio", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void inserisciBilancio(@RequestParam Integer importo) {
+        BigDecimal bil = getBilancio();
+        BigDecimal imp = new BigDecimal(importo);
+        LOG.info("Inserito nuovo bilancio con importo pari a " + imp );
+        bilancioService.modificaBilancio(imp);
+    }
+
+    @RequestMapping(path="/bilancio", method = RequestMethod.GET)
+    public @ResponseBody
+    BigDecimal getBilancio() {
+        //return bilancioService.visualizzaBilancioTotale();
+        return new BigDecimal(57);
     }
 
 }
