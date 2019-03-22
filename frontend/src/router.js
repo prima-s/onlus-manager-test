87b2a7ad9f3ce45c75c114eebc3fa0ef99e2c3f5
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-03-22 11:06:30 +0100

    Updated Router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Service from '@/components/Service'
-import Bootstrap from '@/components/Bootstrap'
-import User from '@/components/User'
+
 
 Vue.use(Router)
 
@@ -18,16 +17,6 @@ export default new Router({
       path: '/callservice',
       name: 'Service',
       component: Service
-    },
-    {
-      path: '/bootstrap',
-      name: 'Bootstrap',
-      component: Bootstrap
-    },
-    {
-      path: '/user',
-      name: 'User',
-      component: User
     }
     
   ]
