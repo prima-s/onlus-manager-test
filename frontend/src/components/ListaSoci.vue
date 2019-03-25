691489e2a7c374afbc4383b4f78189cb028c36da
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-03-25 16:59:25 +0100

    Optimized ListaSoci pdf

--- frontend/src/components/ListaSoci.vue
+++ frontend/src/components/ListaSoci.vue
@@ -1,32 +1,31 @@
 <template>
-    <form class="modal-content" action="">
-        <span  @click="closeAll()" class="close" title="Close Modal">&times;</span>
+  <form class="modal-content" action>
+    <span @click="closeAll()" class="close" title="Close Modal">&times;</span>
     <div class="container">
       <h1>Lista soci</h1>
       <hr>
-   <a href="#" @click="createPDF()" ><i class="far fa-file-pdf"></i>Scarica Pdf ListaSoci </a>
- <br>
- 	<div id="sociTable">
-    <b-table striped hover :items="soci" :fields="fields" />
-  </div>
+      <a href="#" @click="createPDF()">
+        <i class="far fa-file-pdf"></i>Scarica Pdf ListaSoci
+      </a>
+      <br>
+      <div id="sociTable">
+        <b-table striped responsive hover :items="soci" :fields="fields"/>
+      </div>
     </div>
- <br>
+    <br>
 
+    <br>
+    <br>
 
-		<br/><br/>
-
-<div class="row">
+    <div class="row">
       <div class="col-md-6">
-      <button @click="closeAll()" class="cancelbtn btn-block">Annulla</button>
+        <button @click="closeAll()" class="cancelbtn btn-block">Annulla</button>
       </div>
       <div class="col-md-6">
-      <button @click="createPDF()" class="signupbtn btn-block">Download PDF</button>
-			</div>
-</div>
-
-
-
-</form>
+        <button @click="createPDF()" class="signupbtn btn-block">Download PDF</button>
+      </div>
+    </div>
+  </form>
 </template>
 <script scoped>
 import { AXIOS } from "./http-common";
@@ -36,72 +35,77 @@ export default {
       response: [],
       errors: [],
       fields: [
-          {
-            key: 'numTessera',
-            label:'Numero di tessera',
-            sortable:false
-          },
-          {
-            key: 'nome',
-            label: 'Nome',
-            sortable: false
-          },
-          {
-            key: 'cognome',
-            label: 'Cognome',
-            sortable: true
-          }
-         ],
-      soci:null
+        {
+          key: "numTessera",
+          label: "Numero di tessera",
+          sortable: false
+        },
+        {
+          key: "nome",
+          label: "Nome",
+          sortable: false
+        },
+        {
+          key: "cognome",
+          label: "Cognome",
+          sortable: true
+        }
+      ],
+      soci: null
     };
   },
   created() {
-       AXIOS.get(`/soci`)
-               .then(response => {
-                   this.soci=response.data;
-                 })
-                 .catch(e => {
-                   this.errors.push(e)
-                 })
+    AXIOS.get(`/soci`)
+      .then(response => {
+        this.soci = response.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   },
-   mounted() {
-    let recaptchaScript = document.createElement('script');
-    recaptchaScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js');
-    document.head.appendChild(recaptchaScript);
-    },
   methods: {
-       closeAll(){
-      for(var i=0;i<document.getElementsByClassName('modal').length;i++){
-        document.getElementsByClassName('modal')[i].style.display='none';
+    closeAll() {
+      for (
+        var i = 0;
+        i < document.getElementsByClassName("modal").length;
+        i++
+      ) {
+        document.getElementsByClassName("modal")[i].style.display = "none";
       }
     },
-    createPDF () {
-
-    let pdfName = 'test';
-    var doc = new jsPDF();
-    for (var i=0;i<this.soci.length;i++){
-    doc.text(
-    this.soci[i]['numTessera']  + ' -   ' + this.soci[i]['nome'] + '   ' + this.soci[i]['cognome'], 10, 10 + 10*i)
-    }
-    doc.save('ListaSoci' + '.pdf');
-  },
+    createPDF() {
+      var doc = new jsPDF();
+      let pdfName = "Lista Soci";
+      let list = "";
+      let pdf = "";
+      for (var i = 0; i < this.soci.length; i++) {
+        list +=
+          this.soci[i]["numTessera"] +
+          "   -   " +
+          this.soci[i]["nome"] +
+          "    " +
+          this.soci[i]["cognome"] +
+          "\n";
+      }
+      pdf += pdfName + "\n\n";
+      pdf += list;
+      let splitList = doc.splitTextToSize(pdf, 180);
+      doc.text(splitList, 10, 10);
+      doc.save("ListaSoci" + ".pdf");
+    },
 
     // Fetches posts when the component is created.
     retrieveTable() {
       AXIOS.get(`/soci`)
         .then(response => {
-            this.soci=response.data;
-          })
-          .catch(e => {
-            this.errors.push(e)
-          })
+          this.soci = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
     }
-
   }
-
 };
 </script>
 <style scoped>
-
-
 </style>
